a8d6ac03fadc5450e95fc8311dbd920ff46ebc8e
Nantawat6510545543 <nantawat.suk@ku.th>
2024-03-04 15:48:33 +0700

    update base case

--- dot_product_recursive.s
+++ dot_product_recursive.s
@@ -35,22 +35,12 @@ dot_product_recursive:
     addi sp sp -12
 
     # save a0, a1 and ra on to the stack
-    sw a0 8(sp) # Store &a[0]
+    sw a0 8(sp)  # Store &a[0]
     sw a1 4(sp)  # Store &b[0]
     sw ra 0(sp)  # Store ra
 
-    bne a2 t0 return # if size != 1 then return a[0]*b[0] + dot_product_recursive(a+1, b+1, size-1);
+    beq a2 t0 base_case # if size == 1 then base case
 
-    # a0 = a[0] * b[0]
-    lw t1 0(a0)
-    lw t2 0(a1)
-    mul a0 t1 t2
-
-    addi sp sp 12
-    jr ra
-
-return:
-    # a[0]*b[0] + dot_product_recursive(a+1, b+1, size-1);
     addi a0 a0 4  # &a[0] + 4
     addi a1 a1 4  # &b[0] + 4
     addi a2 a2 -1 # size - 1
@@ -58,17 +48,27 @@ return:
     jal dot_product_recursive # dot_product_recursive(a+1, b+1, size-1);
 
     # restore a0, a1 and ra on to the stack
-    lw t1 8(sp) # Load &a[0] (a0)
-    lw t2 4(sp)  # Load &b[0] (a1)
+    lw t1 8(sp)  # Load &a[0]
+    lw t2 4(sp)  # Load &b[0]
     lw ra 0(sp)  # Load ra
 
     # load a[0] and b[0]
     lw t1 0(t1)
     lw t2 0(t2)
 
-    # a0 += a[0] * b[0] | a0 += dot_product_recursive(a+1, b+1, size-1);
+    # a0 += a[0] * b[0]
     mul x20, t1, t2
     add a0 a0 x20
 
     addi sp sp 12
     jr ra
+
+base_case:
+    # return a[0] * b[0]
+    lw t1 0(a0)
+    lw t2 0(a1)
+    mul a0 t1 t2
+
+    addi sp sp 12
+    jr ra
+    
